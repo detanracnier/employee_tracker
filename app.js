03abb205b7c8fca3e808b30ff08f7e1e1c9b2ee3
Spencer Rosio <spencerrosio3d@gmail.com>
2021-01-05 18:24:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection(
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: '!F4ll3n3lite',
+        password: '',
         database: 'employeedb'
     }
 );
